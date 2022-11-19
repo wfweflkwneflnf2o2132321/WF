f4b3649de353b3626e2ee41592c08c6165ec3848
wfweflkwneflnf2o2132321 <artemij.kondr@yandex.ru>
2022-11-20 00:44:14 +0300

    создали новый экран WeatherP.kt создали WeatherP.kt

--- app/src/main/java/com/example/weatherfetcher/MainActivity.kt
+++ app/src/main/java/com/example/weatherfetcher/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.weatherfetcher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val textViewHello = findViewById<TextView>(R.id.tvHello)
         textViewHello.text="Здравствуй жопа новый год"
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+             Intent(this,WA::class.java).also { startActivity(it) }
+
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/example/weatherfetcher/WeatherP.kt
@@ -0,0 +1,7 @@
+package com.example.weatherfetcher
+
+class WeatherP {
+    fun getWeather(temperature:String):String{
+        return temperature
+    }
+}
